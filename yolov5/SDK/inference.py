fc5de8e089b48b0e8eb498a2ee35a7b70b124047
chunjie@aitmed.com <chunjie@aitmed.com>
2021-03-13 23:35:31 +0800

    Update for yolov5

--- yolov5/SDK/inference.py
+++ yolov5/SDK/inference.py
@@ -1,159 +1,179 @@
-import cv2 as cv
-import torch.backends.cudnn as cudnn
 import torch
-import time
-from pathlib import Path
 import numpy as np
+import time
+import face_recognition
 from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages
-from utils.general import check_img_size, check_requirements, non_max_suppression, apply_classifier, scale_coords, \
-    xyxy2xywh, strip_optimizer, set_logging, increment_path
-from utils.plots import plot_one_box
-from utils.torch_utils import select_device, load_classifier, time_synchronized
+from utils.general import non_max_suppression, scale_coords, letterbox
+from utils.torch_utils import select_device
+import cv2 as cv
+from random import randint
 
 
-def detect(source, weights, view_img, img_size, img_name, conf_thres, iou_thres):
-    source, weights, view_img, save_txt, imgsz = source, weights, view_img, False, img_size
+class Detector(object):
+    def __init__(self):
+        self.chunjie_encoding = np.loadtxt("face-encodings/chunjie_encoding",
+                                           delimiter=',')
+        self.yifan_encoding = np.loadtxt("face-encodings/yifan_encoding",
+                                         delimiter=',')
+        self.ruohai_encoding = np.loadtxt("face-encodings/ruohai_encoding",
+                                          delimiter=',')
+        self.name = "Unknown"
+        self.known_face_encoding = [
+            self.chunjie_encoding, self.yifan_encoding, self.ruohai_encoding
+        ]
+        self.known_face_name = ["Chunjie Shan", "Yifan Wei", "Ruohai Hu"]
+        self.img_size = 640
+        self.threshold = 0.6
+        self.max_frame = 160
+        self.init_model()
 
-    webcam = source.isnumeric() or source.endswith(
-        '.txt') or source.lower().startswith(('rtsp://', 'rtmp://', 'http://'))
+    def init_model(self):
+        self.weights = 'weights/best.pt'
+        self.device = '0' if torch.cuda.is_available() else 'cpu'
+        self.device = select_device(self.device)
+        model = attempt_load(self.weights, map_location=self.device)
+        model.to(self.device).eval()
+        model.half()
+        # torch.save(model, 'test.pt')
+        self.m = model
+        self.names = model.module.names if hasattr(model,
+                                                   'module') else model.names
+        self.colors = [(randint(0, 255), randint(0, 255), randint(0, 255))
+                       for _ in self.names]
 
-    # Directories
-    save_dir = Path(
-        increment_path(Path("runs/detect") / img_name))  # increment run
-    (save_dir / 'labels' if save_txt else save_dir).mkdir(
-        parents=True, exist_ok=True)  # make dir
-
-    # Initialize
-    set_logging()
-    device = select_device("0")
-    half = device.type != 'cpu'  # half precision only supported on CUDA
-
-    # Load model
-    model = attempt_load(weights, map_location=device)  # load FP32 model
-    stride = int(model.stride.max())  # model stride
-    imgsz = check_img_size(imgsz, s=stride)  # check img_size
-    if half:
-        model.half()  # to FP16
-
-    # Set Dataloader
-    vid_path, vid_writer = None, None
-    if webcam:
-        view_img = True
-        cudnn.benchmark = True  # set True to speed up constant image size inference
-        dataset = LoadStreams(source, img_size=imgsz, stride=stride)
-    else:
-        save_img = True
-        dataset = LoadImages(source, img_size=imgsz, stride=stride)
-
-    # Get names and colors
-    names = model.module.names if hasattr(model, 'module') else model.names
-    colors = [[np.random.randint(0, 255) for _ in range(3)] for _ in names]
-
-    # Run inference
-    if device.type != 'cpu':
-        model(
-            torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(
-                next(model.parameters())))  # run once
-    t0 = time.time()
-    for path, img, im0s, vid_cap in dataset:
-        img = torch.from_numpy(img).to(device)
-        img = img.half() if half else img.float()  # uint8 to fp16/32
-        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+    def preprocess(self, img):
+        img0 = img.copy()
+        img = letterbox(img, new_shape=self.img_size)[0]
+        img = img[:, :, ::-1].transpose(2, 0, 1)
+        img = np.ascontiguousarray(img)
+        img = torch.from_numpy(img).to(self.device)
+        img = img.half() # 半精度
+        img /= 255.0 # 图像归一化
         if img.ndimension() == 3:
             img = img.unsqueeze(0)
 
-        # Inference
-        t1 = time_synchronized()
-        pred = model(img, augment=False)[0]
-        print("Prediction's shape: ", pred.shape)
+        return img0, img
 
-        # Apply NMS
-        pred = non_max_suppression(pred,
-                                   conf_thres=conf_thres,
-                                   iou_thres=iou_thres,
-                                   classes=None,
-                                   agnostic=False)
-        t2 = time_synchronized()
+    def plot_bboxes(self, image, bboxes, line_thickness=None):
+        tl = line_thickness or round(
+            0.002 *
+            (image.shape[0] + image.shape[1]) / 2) + 1 # line/font thickness
+        for (x1, y1, x2, y2, cls_id, conf) in bboxes:
+            color = self.colors[self.names.index(cls_id)]
+            c1, c2 = (x1, y1), (x2, y2)
+            cv.rectangle(image,
+                         c1,
+                         c2,
+                         color,
+                         thickness=tl,
+                         lineType=cv.LINE_AA)
+            tf = max(tl - 1, 1) # font thickness
+            t_size = cv.getTextSize(cls_id, 0, fontScale=tl / 3,
+                                    thickness=tf)[0]
+            c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+            cv.rectangle(image, c1, c2, color, -1, cv.LINE_AA) # filled
+            cv.putText(image,
+                       '{}'.format(cls_id), (c1[0], c1[1] - 2),
+                       0,
+                       tl / 3, [225, 255, 255],
+                       thickness=tf,
+                       lineType=cv.LINE_AA)
+        return image
 
-        # Process detections
-        for i, det in enumerate(pred):  # detections per image
-            if webcam:  # batch_size >= 1
-                p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(
-                ), dataset.count
-            else:
-                p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+    def recognition(self, image, bboxes, line_thickness=None):
+        tl = line_thickness or round(
+            0.002 *
+            (image.shape[0] + image.shape[1]) / 2) + 1 # line/font thickness
 
-            p = Path(p)  # to Path
-            save_path = str(save_dir / p.name)  # img.jpg
-            txt_path = str(save_dir / 'labels' / p.stem) + (
-                '' if dataset.mode == 'image' else f'_{frame}')  # img.txt
-            s += '%gx%g ' % img.shape[2:]  # print string
-            gn = torch.tensor(im0.shape)[[1, 0, 1,
-                                          0]]  # normalization gain whwh
-            if len(det):
-                # Rescale boxes from img_size to im0 size
+        tf = max(tl - 1, 1) # font thickness
+        for (x1, y1, x2, y2, cls_id, conf) in bboxes:
+            face_location = [(y1, x2, y2, x1)]
+            face_encoding = face_recognition.face_encodings(
+                image, face_location)
+            if face_location:
+                for (y1, x2, y2,
+                     x1), face_encoding in zip(face_location, face_encoding):
+                    matches = face_recognition.compare_faces(
+                        self.known_face_encoding, face_encoding)
+                    face_distances = face_recognition.face_distance(
+                        self.known_face_encoding, face_encoding)
+                    best_match_index = np.argmin(face_distances)
+                    if matches[best_match_index]:
+                        self.name = self.known_face_name[best_match_index]
+                    else:
+                        self.name = "Unknown"
+                    t_size = cv.getTextSize(self.name,
+                                            0,
+                                            fontScale=tl / 3,
+                                            thickness=tf)[0]
+                    c1, c2 = (x1, y2), (x2, y1)
+                    c2 = c1[0] + t_size[0], c1[1] + t_size[1] + 3
+                    cv.rectangle(image, c1, c2, (0, 0, 255), -1,
+                                 cv.LINE_AA) # filled
+                    image = cv.putText(image,
+                                       self.name, (x1, y2 + 20),
+                                       0,
+                                       tl / 3, (255, 255, 255),
+                                       thickness=2)
+
+        return image
+
+    def detect(self, im):
+
+        im0, img = self.preprocess(im)
+
+        curr_time = time.time()
+        pred = self.m(img, augment=False)[0]
+        pred = pred.float()
+        pred = non_max_suppression(pred, self.threshold, 0.3)
+
+        pred_boxes = []
+        non_mask_boxes = []
+        image_info = {}
+        count = 0
+        for det in pred:
+            if det is not None and len(det):
                 det[:, :4] = scale_coords(img.shape[2:], det[:, :4],
                                           im0.shape).round()
 
-                # Print results
-                for c in det[:, -1].unique():
-                    n = (det[:, -1] == c).sum()  # detections per class
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+                for *x, conf, cls_id in det:
+                    lbl = self.names[int(cls_id)]
+                    x1, y1 = int(x[0]), int(x[1])
+                    x2, y2 = int(x[2]), int(x[3])
+                    pred_boxes.append((x1, y1, x2, y2, lbl, conf))
+                    if cls_id == 1:
+                        non_mask_boxes.append((x1, y1, x2, y2, lbl, conf))
+                    count += 1
+                    key = '{}-{:02}'.format(lbl, count)
+                    image_info[key] = [
+                        '{}×{}'.format(x2 - x1, y2 - y1),
+                        np.round(float(conf), 3)
+                    ]
 
-                # Write results
-                save_conf = False
-                for *xyxy, conf, cls in reversed(det):
-                    if save_txt:  # Write to file
-                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) /
-                                gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if save_conf else (
-                            cls, *xywh)  # label format
-                        with open(txt_path + '.txt', 'a') as f:
-                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+        im = self.plot_bboxes(im, pred_boxes)
+        if len(non_mask_boxes) > 0:
+            im = self.recognition(im, non_mask_boxes)
+        infer_time = time.time() - curr_time
+        print("The inference time is {:.2f} ms".format(infer_time * 1000))
+        return im, image_info
 
-                    if save_img or view_img:  # Add bbox to image
-                        label = f'{names[int(cls)]} {conf:.2f}'
-                        plot_one_box(xyxy,
-                                     im0,
-                                     label=label,
-                                     color=colors[int(cls)],
-                                     line_thickness=3)
 
-            # Print time (inference + NMS)
-            print(f'{s}Done. ({t2 - t1:.3f}s)')
+if __name__ == "__main__":
+    detector = Detector()
+    cap = cv.VideoCapture(0)
 
-            # Stream results
-            if view_img:
-                cv.imshow(str(p), im0)
-                cv.waitKey(0)  # 1 millisecond
-
-            # Save results (image with detections)
-            if save_img:
-                if dataset.mode == 'image':
-                    cv.imwrite(save_path, im0)
-                else:  # 'video'
-                    if vid_path != save_path:  # new video
-                        vid_path = save_path
-                        if isinstance(vid_writer, cv.VideoWriter):
-                            vid_writer.release(
-                            )  # release previous video writer
-
-                        fourcc = 'mp4v'  # output video codec
-                        fps = vid_cap.get(cv.CAP_PROP_FPS)
-                        w = int(vid_cap.get(cv.CAP_PROP_FRAME_WIDTH))
-                        h = int(vid_cap.get(cv.CAP_PROP_FRAME_HEIGHT))
-                        vid_writer = cv.VideoWriter(
-                            save_path, cv.VideoWriter_fourcc(*fourcc), fps,
-                            (w, h))
-                    vid_writer.write(im0)
-
-    if save_txt or save_img:
-        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
-        print(f"Results saved to {save_dir}{s}")
-
-    print(f'Done. ({time.time() - t0:.3f}s)')
-
-if __name__ == '__main__':
-    with torch.no_grad():
-        detect("data/brain/images/valid/", "models/brain/best.pt", True, 640, "exp", 0.25, 0.45)
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            frame, info = detector.detect(frame)
+            cv.imshow("Video", frame)
+            c = cv.waitKey(1)
+            if c == 27:
+                break
+        else:
+            break
+    cap.release()
+    # image = cv.imread("./data/brain/images/train/030.png")
+    # image, image_info = detector.detect(image)
+    # cv.imshow("Frame", image)
+    # cv.waitKey(0)
